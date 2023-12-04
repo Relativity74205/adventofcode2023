2a17050e6839a0d964279286db0a2c5d68ea8e14
Arkadius Schuchhardt <arkadius@schuchhardt.com>
2023-12-04 17:53:22 +0100

    feat: day04

---
+++ src/main/kotlin/day04.kt
@@ -0,0 +1,44 @@
+import kotlin.math.pow
+
+private class Card(var copies: Int, val winningNumbers: Set<Int>, val numbers: Set<Int>) {
+    fun matches(): Int {
+        return this.numbers.intersect(this.winningNumbers).size
+    }
+}
+
+
+private fun parseLines(lines: List<String>): List<Card> {
+    val cards = mutableListOf<Card>()
+    for (line in lines) {
+        val winningNumbers = line.split(": ")[1].split(" | ")[0].trim().split("  ", " ").map { it.trim().toInt() }
+        val numbers = line.split(": ")[1].split(" | ")[1].trim().split("  ", " ").map { it.trim().toInt() }
+        cards.add(Card(1, winningNumbers.toSet(), numbers.toSet()))
+    }
+    return cards.toList()
+}
+
+private fun partA(lines: List<String>): Int {
+    val cards = parseLines(lines)
+    return cards.sumOf { (2.0).pow(it.matches() - 1).toInt() }
+}
+
+private fun partB(lines: List<String>): Int {
+    val cards = parseLines(lines)
+    for (i in cards.indices) {
+        val card = cards[i]
+        for (j in 1..card.matches()) {
+            cards[i + j].copies += card.copies
+        }
+    }
+    return cards.sumOf { it.copies }
+}
+
+fun main() {
+    val lines = readFile(4, false)
+    val linesDebug = readFile(4, true)
+
+    println("Part A Debug: ${partA(linesDebug)}")
+    println("Part A: ${partA(lines)}")
+    println("Part B Debug: ${partB(linesDebug)}")
+    println("Part B: ${partB(lines)}")
+}
