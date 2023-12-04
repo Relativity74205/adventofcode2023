27a827df28202223bbccc9141774507d00aa5e8e
Arkadius Schuchhardt <arkadius@schuchhardt.com>
2023-12-04 16:51:21 +0100

    refactor: day03a

--- src/main/kotlin/day03.kt
+++ src/main/kotlin/day03.kt
@@ -1,34 +1,37 @@
 import kotlin.math.max
 import kotlin.math.min
 
-private class PartNumber (val number: Int, val x: Int, val y: Int)
+private class Number (val value: Int, val x: Int, val y: Int)
+private class Symbol (val symbol: Char, val x: Int, val y: Int)
 
 
-private fun getNumbers(lines: List<String>): List<PartNumber> {
-    val numbers = mutableListOf<PartNumber>()
+private fun getNumbers(lines: List<String>): Pair<List<Number>, List<Symbol>> {
+    val numbers = mutableListOf<Number>()
+    val symbols = mutableListOf<Symbol>()
     for (y in lines.indices) {
         val line = lines[y]
-        var number = ""
-        for (x in line.indices) {
-            if (line[x] in '0'..'9') {
-               number += line[x]
-            } else {
-                if (number != "") {
-                    numbers.add(PartNumber(number.toInt(), x - number.length, y))
-                    number = ""
-                }
+        var x = 0
+        while (x < line.length ) {
+            if (line[x] !in '0'..'9' && line[x] != '.') {
+                symbols.add(Symbol(line[x], x, y))
             }
-        }
-        if (number != "") {
-            numbers.add(PartNumber(number.toInt(), line.length - number.length, y))
+            if (line[x] in '0'..'9') {
+                var number = line[x].toString()
+                while (x + 1 < line.length && line[x + 1] in '0'..'9') {
+                    number += line[x + 1]
+                    x++
+                }
+                numbers.add(Number(number.toInt(), x - number.length + 1, y))
+            }
+            x++
         }
     }
-    return numbers.toList()
+    return numbers.toList() to symbols.toList()
 }
 
-private fun checkPartNumber(number: PartNumber, lines: List<String>): Boolean {
+private fun checkPartNumber(number: Number, lines: List<String>): Boolean {
     for (y in max(number.y - 1, 0) .. min(number.y + 1, lines.size - 1)) {
-        for (x in max(number.x - 1, 0) .. min(number.x + number.number.toString().length,lines[y].length - 1)) {
+        for (x in max(number.x - 1, 0) .. min(number.x + number.value.toString().length,lines[y].length - 1)) {
             if (lines[y][x] !in '0'..'9' && lines[y][x] != '.') {
                 return true
             }
@@ -38,10 +41,9 @@ private fun checkPartNumber(number: PartNumber, lines: List<String>): Boolean {
 }
 
 private fun partA(lines: List<String>): Int {
-    val numbers = getNumbers(lines)
+    val (numbers, symbols) = getNumbers(lines)
     val partNumbers = numbers.filter { checkPartNumber(it, lines) }
-    val notPartNumbers = numbers.filter { !checkPartNumber(it, lines) }
-    return partNumbers.sumOf { it.number }
+    return partNumbers.sumOf { it.value }
 }
 
 private fun partB(lines: List<String>): Int{
