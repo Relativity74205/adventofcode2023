607f50b98e780efbdb8bd4d04c6447a1f8bae602
Arkadius Schuchhardt <arkadius@schuchhardt.com>
2023-12-14 22:42:40 +0100

    feat: day11

--- src/main/kotlin/day11.kt
+++ src/main/kotlin/day11.kt
@@ -1,43 +1,47 @@
+import java.math.BigInteger
 import kotlin.math.abs
 
 private class GalaxyCoordinates (val x: Int, val y: Int) {
-    fun distanceTo(other: GalaxyCoordinates): Int {
-        return abs(this.x - other.x) + abs(this.y - other.y)
+    fun distanceTo(other: GalaxyCoordinates): BigInteger {
+        return abs(this.x - other.x).toBigInteger() + abs(this.y - other.y).toBigInteger()
     }
 }
 
 
-private fun getGalaxies(lines: List<String>): List<GalaxyCoordinates> {
+private fun getGalaxies(lines: List<String>, factor: Int = 2): List<GalaxyCoordinates> {
     val emptyRows = lines.mapIndexed { index, line -> if (line.all { char -> char == '.' }) index else null }.filterNotNull()
     val emptyColumns =
         lines.indices.mapNotNull { index -> if (lines.all { line -> line[index] == '.' }) index else null }
 
-    val newGalaxyMap = lines.toMutableList()
-    for (i in emptyRows.sorted().reversed()) {
-        newGalaxyMap.add(i, ".".repeat(lines[0].length))
-    }
-    for (i in emptyColumns.sorted().reversed()) {
-        newGalaxyMap.forEachIndexed { index, line ->
-            newGalaxyMap[index] = line.substring(0, i) + "." + line.substring(i)
-        }
+    val galaxyMap = lines.mapIndexed{y, line -> line.mapIndexed { x, char -> if (char == '#') GalaxyCoordinates(x, y) else null }.filterNotNull() }.flatten()
+    val newGalaxyMap = mutableListOf<GalaxyCoordinates>()
+    for (galaxy in galaxyMap) {
+        val deltaXShift = emptyColumns.sumOf { emptyColumn -> if (galaxy.x > emptyColumn) factor - 1 else 0 }
+        val deltaYShift = emptyRows.sumOf { emptyRow -> if (galaxy.y > emptyRow) factor - 1 else 0 }
+        newGalaxyMap.add(GalaxyCoordinates(galaxy.x + deltaXShift, galaxy.y + deltaYShift))
     }
 
-    return newGalaxyMap.mapIndexed{x, line -> line.mapIndexed { y, char -> if (char == '#') GalaxyCoordinates(x, y) else null }.filterNotNull() }.flatten()
+    return newGalaxyMap
 }
 
-private fun partA(lines: List<String>): Int {
-    val galaxies = getGalaxies(lines)
-    val paths = mutableListOf<Int>()
+private fun getPathLengths(galaxies: List<GalaxyCoordinates>): List<BigInteger> {
+    val paths = mutableListOf<BigInteger>()
     for (index in galaxies.indices) {
         for (otherIndex in index + 1..<galaxies.size) {
             paths.add(galaxies[index].distanceTo(galaxies[otherIndex]))
         }
     }
-    return paths.sum()
+    return paths
 }
 
-private fun partB(lines: List<String>): Int {
-    return 0
+private fun partA(lines: List<String>): BigInteger {
+    val galaxies = getGalaxies(lines)
+    return getPathLengths(galaxies).reduce(BigInteger::add)
+}
+
+private fun partB(lines: List<String>): BigInteger {
+    val galaxies = getGalaxies(lines, 1000000)
+    return getPathLengths(galaxies).reduce(BigInteger::add)
 }
 
 fun main() {
